f6e0d60e11518393df4e0ab5e85779ee16b7050b
Carlos Nayi <carlos.nayi@mindfactory.ar>
2025-11-20 09:11:51 -0300

    feat:Add connection to backend and structure of proyect's folders

---
+++ mfnews-frontend/src/api/newsApi.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+//Aca nos conectamos al back end. Si cambia la URL, solo se modifica aca y listo
+const newsApi = axios.create({
+    baseURL: 'http://localhost:3000/api/news'
+});
+
+
+newsApi.interceptors.response.use(
+    response => response,
+    error => {
+        console.error("Error en la API:", error);
+        return Promise.reject(error);
+    }
+);
+
+
+/*
+El inteceptor es clave y hay varios tipos(este es una de respuesta)
+basicamente son funciones que te permiten interceptar y modificar HTTP requests
+o respondes ANTES de que sean manejadas por un then y catch
+
+Es como un middleware :)
+
+*/
+
+
+export default newsApi;
